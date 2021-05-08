cf4ff4bf87e194c7adda0f17b9a308695da80d4d
Artem Zagarov <artem.zagarov@gmail.com>
2021-05-08 12:23:07 +0300

    Added Modal container
    Added React-router to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 export const App = () => {
-	return <div></div>
+	return <Router></Router>
 }
---
+++ src/components/Modals/Modal.tsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react'
+import { createPortal } from 'react-dom'
+import { useHistory } from 'react-router-dom'
+import usePortal from '../../services/hooks/usePortal'
+
+const Modal = ({ children }: { children: HTMLElement }) => {
+	const target = usePortal()
+	let history = useHistory()
+
+	useEffect((): any => {
+		document.body.style.overflow = 'hidden'
+		return () => (document.body.style.overflow = 'unset')
+	}, [])
+
+	return createPortal(
+		<div
+			className='fixed top-0 bottom-0 left-0 right-0 z-20 flex items-center justify-center w-full h-full text-xs bg-black bg-opacity-40 lg:text-lg'
+			onClick={() => history.goBack()}
+		>
+			{children}
+		</div>,
+		target
+	)
+}
+
+export default Modal
