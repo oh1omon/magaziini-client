1aa171ebaea37583309aef4fd3a7cc0a44358c78
Artem Zagarov <artem.zagarov@gmail.com>
2021-05-17 00:30:38 +0300

    changed back button by one pixel

--- src/components/BackBtn.tsx
+++ src/components/BackBtn.tsx
@@ -6,7 +6,7 @@ export const BackBtn = ({ backHandler }: IBackBtnProps) => {
 		<div className={'md:absolute fixed bottom-6 right-6 md:top-12 md:left-12 z-10 w-auto h-auto text-lg'}>
 			<button
 				onClick={backHandler}
-				className={'py-2 px-4 border border-black bg-white hover:bg-gray-200 flex flex-row items-center'}
+				className={'py-2 px-4 border-2 border-black bg-white hover:bg-gray-200 flex flex-row items-center'}
 			>
 				<Back /> Back
 			</button>
